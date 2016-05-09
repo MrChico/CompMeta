c40e6ebba1c5353c6a589554648123daa6f9b28b
Denis Erfurt <denis.erfurt@gmail.com>
2016-05-09 15:43:04 +0200

    3. and fuck 4.

--- as03/Scratch.thy
+++ as03/Scratch.thy
@@ -15,7 +15,7 @@ proof -
     then  have px: "P x" by (rule allE)
     from a have "\<forall> x. Q x" by (rule conjunct2)
     then have "Q x" by (rule allE)
-    with px have "P x \<and> Q x" by (rule conjI)    
+    with px have "P x \<and> Q x" by (rule conjI)
   }
   thus ?thesis by (rule allI)
 qed
@@ -78,7 +78,6 @@ subsection \<open>Exercise 2\<close>
 
 theorem "a":
   shows "(\<exists> x. \<forall> y. P x y) \<longrightarrow> (\<forall> y. \<exists> x. P x y)"
-  nitpick
 proof -
   {
     assume a: "\<exists> x. \<forall> y. P x y"
@@ -109,7 +108,6 @@ oops
 lemma "c":
   shows "((\<forall> x. P x) \<or> (\<forall> x. Q x)) \<longleftrightarrow> (\<forall> x. (P x \<or> Q x))"
 proof -
-nitpick
   (*
     'a = {a, b}
     P(a) = False
@@ -121,7 +119,6 @@ oops
 
 lemma "d":
   shows "((\<exists> x. P x) \<or> (\<exists> x. Q x)) \<longleftrightarrow> (\<exists> x. (P x \<or> P x))"
-  nitpick
   (*
     'a = {a,b,c}
     P(a) = False
@@ -135,7 +132,6 @@ oops
 
 lemma "e":
   shows "(\<forall> x. \<exists> y. P x y) \<longrightarrow> (\<exists> y. \<forall> x. P x y)"
-  nitpick
   (*
     'a = {a,b}
     P(a,a) = False
@@ -181,3 +177,80 @@ proof -
   from ltor rtol have "(\<not>(\<forall> x. P x)) \<longleftrightarrow> (\<exists> x. \<not> P x)" by (rule iffI)
   thus ?thesis .
 qed
+
+subsection \<open>Exercise 3\<close>
+
+theorem "3a":
+  shows "(\<exists> x. \<forall> y. P x y) \<longrightarrow> (\<forall> y. \<exists> x. P x y)"
+proof -
+  {
+    assume a: "\<exists> x. \<forall> y. P x y"
+    {
+      fix y
+      from a obtain x where 1: "\<forall> y. P x y" by (rule exE)
+      hence "P x y" by (rule allE)
+      hence "\<exists> x. P x y" by (rule exI)
+    }
+    hence "\<forall> y. \<exists> x. P x y" by (rule allI)
+  }
+  thus ?thesis by (rule impI)
+qed
+
+lemma "3f":
+  shows "(\<not>(\<forall> x. P x)) \<longleftrightarrow> (\<exists> x. \<not> P x)"
+proof
+  assume a: "(\<not>(\<forall> x. P x))"
+  show "\<exists> x. \<not> P x"
+  proof (rule ccontr)
+    assume b: "\<not>(\<exists> x. \<not> P x)"
+    {
+        fix y
+        {
+          assume "\<not>P y"
+          hence "\<exists> x. \<not> P x" by (rule exI)
+          with b have "False" by (rule notE)
+        }
+        hence "P y" by (rule ccontr)
+      }
+      hence "\<forall> x. P x" by (rule allI)
+      with a show "False" by (rule notE)
+  qed
+next
+  assume 1: "\<exists> x. \<not> P x"
+  show "\<not>(\<forall> x. P x)"
+  proof (rule notI)
+    from 1 obtain a where 2: "\<not>P a" by (rule exE)
+    assume "\<forall> x. P x"
+    hence "P a" by (rule allE)
+    with 2 show "False" by (rule notE)
+  qed
+qed
+
+subsection \<open>Exercise 4\<close>
+
+lemma allDeMorgan: "\<not> (\<forall> x. P(x)) \<Longrightarrow> (\<exists> x. \<not> (P(x)))" by simp
+
+lemma
+  shows a: "(\<exists> x. (D(x)) \<longrightarrow> (\<forall> y. D(y)))"
+proof cases
+  assume a: "\<forall> y. D(y)"  
+  {
+    assume "(\<exists> x. D(x))"
+    from a have "(\<forall> y. D(y))" by -
+  }
+  from this have ?thesis by (rule impI) (* why the fuck not? *)
+  thus ?thesis .
+next
+  assume "\<not> (\<forall> y. D(y))"
+  then have "\<exists> y. \<not> (D(y))" by (rule allDeMorgan)
+  from this obtain y where 3: "\<not> D(y)"
+  (* if there is someone (y) who is not drinking, then D(y) \<longrightarrow> \<forall> x. D(x) is true *)
+  (*
+  assume D(y)
+  False
+  \<Rightarrow> ?Thesis (from false everything follows)
+  *)
+  sorry
+qed
+
+end
