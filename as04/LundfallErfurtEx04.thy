9424e780095c1330be06554bec31bb4b1122f326
Denis Erfurt <denis.erfurt@gmail.com>
2016-05-15 16:49:24 +0200

    add ex2

--- as04/LundfallErfurtEx04.thy
+++ as04/LundfallErfurtEx04.thy
@@ -45,6 +45,51 @@ text \<open>One argument could be that we do not accept the law of double negati
   use it in everyday language. The assumption 'it is not that case that (if I pray, my prayers will
   be answered)' is different from 'if I pray, my prayers will not be answered', which is probably
   a more reasonable assumption in the context of gods nonexistence.\<close>  
+
+
+text \<open>Ex 2\<close>
+
+fun sum_n :: "nat \<Rightarrow> nat" where
+  "sum_n 0 = 0" |
+  "sum_n (Suc n) = Suc n + sum_n n"
+
+lemma "sum_n n = n * (n + 1) div 2"
+
+proof (induction n)
+  case 0
+  show ?case by simp
+next
+  case (Suc n)
+  from this have "sum_n (Suc n) = (Suc n) * ((Suc n) + 1) div 2" by simp
+  thus ?case .
+qed
+
+fun sum_n_square :: "nat \<Rightarrow> nat" where
+  "sum_n_square 0 = 0" |
+  "sum_n_square (Suc n) = Suc n * Suc n + sum_n_square n"
+
+lemma "sum_n_square n = (n * (n + 1) * (2 * n + 1)) div 6"
+proof (induction n)
+  case 0
+  show ?case by simp
+next
+  case (Suc n)
+  from this have "sum_n_square (Suc n) = Suc n * Suc n + sum_n_square n" by simp
+  (* how to remove the div 6 here ? *)
+  then have "sum_n_square (Suc n) = Suc n * Suc n + (n * (n + 1) * (2 * n + 1)) div 6" by (simp add: Suc.IH)
+  then have "sum_n_square (Suc n) = ((Suc n) * (n + 1) * 6 + (Suc n) * n * (2 * n + 1)) div 6" by simp
+  (* dafuck simp can't distributive rule -- fuck you metis *)
+  then have "sum_n_square (Suc n) = ((Suc n) * ((n + 1) * 6 + n * (2 * n + 1))) div 6" by (metis add_mult_distrib2 mult.assoc)
+  then have "sum_n_square (Suc n) = ((Suc n) * ((2 * n + 3) * 2 + 2 * n + n * (2 * n + 1))) div 6" by simp
+  then have "sum_n_square (Suc n) = ((Suc n) * ((2 * n + 3) * 2 + n * (2 * n + 3))) div 6" using add_mult_distrib2 by simp
+  then have "sum_n_square (Suc n) = ((Suc n) * (2 * n + 3) * (n + 2)) div 6" by (simp add: add_mult_distrib2 mult.commute)
+  then have "sum_n_square (Suc n) = (Suc n * (Suc n + 1) * (2 * Suc n + 1)) div 6" by (metis One_nat_def Suc_1 add_Suc_right add_Suc_shift mult_2 numeral_3_eq_3 semiring_normalization_rules(16))
+  thus ?case .
+qed
+
+
+
+
 theorem test:
   assumes "\<not>A \<longrightarrow> B" 
   shows "A \<or> B" 
