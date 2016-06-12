0ad94dd98a1f80f45cc4152d382a26d91a7861d4
Denis Erfurt <denis.erfurt@gmail.com>
2016-06-12 20:00:44 +0200

    soo much wow

--- as08/LundfallErfurtEx08.thy
+++ as08/LundfallErfurtEx08.thy
@@ -25,4 +25,53 @@ Nitpick found a counterexample for card i = 2:
 oops
 section \<open>Exercise 2\<close>
 
+theorem itoii:
+  assumes "transitive"
+  shows "\<forall>\<phi>.\<lfloor>\<^bold>\<box>\<phi>\<^bold>\<rightarrow>\<^bold>\<box>\<^bold>\<box>\<phi>\<rfloor>"
+proof -
+  show ?thesis using assms by blast
+qed
+
+
+theorem iitoiii:
+  assumes "\<forall>\<phi>.\<lfloor>\<^bold>\<box>\<phi>\<^bold>\<rightarrow>\<^bold>\<box>\<^bold>\<box>\<phi>\<rfloor>"
+  shows "\<lfloor>\<^bold>\<box>\<^bold>\<top>\<^bold>\<rightarrow>\<^bold>\<box>\<^bold>\<box>\<^bold>\<top>\<rfloor>"
+proof -
+show ?thesis using assms by blast
+qed
+
+theorem iitoi:
+  assumes "\<forall>\<phi>.\<lfloor>\<^bold>\<box>\<phi>\<^bold>\<rightarrow>\<^bold>\<box>\<^bold>\<box>\<phi>\<rfloor>"
+  shows "transitive"
+proof -
+show ?thesis using assms by blast
+qed
+
+theorem iiitoi:
+  assumes "\<lfloor>\<^bold>\<box>\<^bold>\<top>\<^bold>\<rightarrow>\<^bold>\<box>\<^bold>\<box>\<^bold>\<top>\<rfloor>"
+  shows "\<forall>\<phi>.\<lfloor>\<^bold>\<box>\<phi>\<^bold>\<rightarrow>\<^bold>\<box>\<^bold>\<box>\<phi>\<rfloor>"
+sorry
+
+
+theorem omfg:
+  assumes "\<lfloor>p\<rfloor>" (* \<forall>w. p(w) *)
+  shows "\<lfloor>\<^bold>\<box>p\<rfloor>"  (* \<forall>w. \<forall> v. ( w r v \<^bold>\<rightarrow> p(v) ) *)
+proof -
+show ?thesis by (simp add: assms)
+qed
+
+theorem omfg2:
+  shows "\<lfloor>p\<^bold>\<rightarrow>\<^bold>\<box>p\<rfloor>"
+  nitpick
+(*
+  Free variable:
+    p = (\<lambda>x. _)(i\<^sub>1 := False, i\<^sub>2 := True)
+  Skolem constants:
+    v = i\<^sub>1
+    w = i\<^sub>2
+*)
+oops
+
+
+
 end
