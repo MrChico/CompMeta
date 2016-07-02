5cd80d8d357142b5a83af24d4a5a8de5aba140bb
Denis Erfurt <denis.erfurt@gmail.com>
2016-07-02 16:29:49 +0200

    congruence axiomatisation

--- proj/spatial.thy
+++ proj/spatial.thy
@@ -18,17 +18,36 @@ value "\<^bold>0\<parallel>\<^bold>0"
 value "\<acute>zero`"
 value "zero\<triangleleft>\<^bold>0\<triangleright>"
 
-(*Structural congruence*)
-axiomatization where
-  Id: "\<^bold>0 \<parallel> p = p" and
-  Sym: "P \<parallel> Q = Q \<parallel> P" and
-  Assoc: "(P \<parallel> Q) \<parallel> R = P \<parallel> (Q \<parallel> R)" 
+
+consts conguence :: "P \<Rightarrow> P \<Rightarrow> bool" (infix "=C" 42)
+
+abbreviation reflexive
+  where "reflexive \<equiv> \<forall> r. r =C r"
+
+abbreviation transitive
+  where "transitive \<equiv> \<forall> x. \<forall> y. \<forall> z. x =C y \<and> y =C z \<longrightarrow> x =C z"
+
+abbreviation symmetric
+  where "symmetric \<equiv> \<forall> x. \<forall> y. x =C y \<longrightarrow> y =C x"
+
+abbreviation Id 
+  where "Id \<equiv> \<forall> p. (\<^bold>0 \<parallel> p) =C p"
+
+abbreviation Sym
+  where "Sym \<equiv> \<forall> p. \<forall> q. p \<parallel> q =C q \<parallel> p"
+
+abbreviation Assoc
+  where "Assoc \<equiv> \<forall> p. \<forall> q. \<forall> r. (p \<parallel> q) \<parallel> r =C p \<parallel> (q \<parallel> r)"
+
+axiomatization where leastConguence: "reflexive \<and> transitive \<and> symmetric \<and> Id \<and> Sym \<and> Assoc"
 
 theorem test:
-  assumes "Id" and "Sym"
-  shows "p \<parallel> \<^bold>0 = p"
-  
+  shows "\<forall> p.(p \<parallel> \<^bold>0) =C p"
+using leastConguence by blast
 
+theorem testtie:
+  shows "p \<parallel> (\<^bold>0 \<parallel> q) =C q \<parallel> p"  
+by (meson leastConguence)
 (*Name equivalence*)
 
 end
