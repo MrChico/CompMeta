5e25a66fae4411bcab7d09b6b95b7c89f88000c2
Denis Erfurt <denis.erfurt@gmail.com>
2016-07-02 15:01:55 +0200

    add congruence

--- proj/spatial.thy
+++ proj/spatial.thy
@@ -2,7 +2,22 @@ theory spatial
 imports Main
 begin
 
-datatype P = Null | input n n P | lift n P | drop n | par P P  
-  and n = quote P
+datatype P = Null             ("\<^bold>0")
+           | input n n P      ("_\<leftarrow>_._" 10)
+           | lift n P         ("_\<triangleleft>_\<triangleright>" 20)
+           | drop n           ("\<acute>_`" 30)
+           | par P P          (infixl "\<parallel>" 30)    
+     and n = quote P          ("`_\<acute>")
 
+abbreviation zero :: n
+  where "zero \<equiv> `\<^bold>0\<acute>"
+
+value "zero \<leftarrow> zero.\<^bold>0"
+value "\<^bold>0\<parallel>\<^bold>0"
+value "\<acute>zero`"
+value "zero\<triangleleft>\<^bold>0\<triangleright>"
+
+consts SE :: "P \<Rightarrow> P \<Rightarrow> bool"
+axiomatization where
+  STREQ: "SE (\<^bold>0\<parallel>p) p" 
 end
