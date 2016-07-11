13e4c53a52c618cce6e45694afa0cd0e4056daad
Denis Erfurt <denis.erfurt@gmail.com>
2016-07-11 22:10:57 +0200

    improved some things, but i'm still not happy

--- proj/spatial.thy
+++ proj/spatial.thy
@@ -22,8 +22,11 @@ termination
   proof - qed
 
 (*Syntactic sugar for output on a channel*)
-abbreviation Output :: "n \<Rightarrow> n \<Rightarrow> P" ("_[_]")
+abbreviation Output :: "n \<Rightarrow> n \<Rightarrow> P" ("_[[_]]")
   where "Output x y \<equiv> x\<triangleleft>\<acute>y`\<triangleright>"
+
+value "a[[b]]"
+
 
 abbreviation zero :: n
   where "zero \<equiv> `\<^bold>0\<acute>"
@@ -41,13 +44,19 @@ where
  |"getList (a\<parallel>b) = ((getList a)@(getList b))"
  |"getList a = [a]"
 
+(*
+fun la :: "P \<Rightarrow> P"
+where
+  "la (a\<parallel>(b\<parallel>c)) = ((la (a\<parallel>b))\<parallel>(la c))"
+ |"la a = a"
 
-
+value "la (Null \<parallel> (Null \<parallel> (Null \<parallel> (Null \<parallel> Null))))"
+value "la ((Null \<parallel> Null) \<parallel> (Null \<parallel> (Null \<parallel> Null)))"
+*)
 
 function congru :: "P \<Rightarrow> P \<Rightarrow> bool" (infixl "=C" 42)
+     and eq2 :: "P list \<Rightarrow> P list \<Rightarrow> P list \<Rightarrow> bool"
      and eq :: "P list \<Rightarrow> P list \<Rightarrow> bool"
-     and lminus :: "P \<Rightarrow> P list \<Rightarrow> P list"
-     and inC :: "P \<Rightarrow> P list \<Rightarrow> bool"
 where
    "congru (a\<parallel>b) (c\<parallel>d)       = (eq (getList (a\<parallel>b)) (getList (c\<parallel>d)))"
   |"congru (x\<leftarrow>y.(a)) (xx\<leftarrow>yy.(b)) = ((a =C b) \<and> (x=xx) \<and> (y = yy))"
@@ -63,7 +72,6 @@ where
   |"congru (a\<parallel>b) (\<acute>c`)       = (eq (getList (a\<parallel>b)) ((\<acute>c`)#[]))"
   |"congru (\<acute>c`) (a\<parallel>b)       = (eq (getList (a\<parallel>b)) ((\<acute>c`)#[]))"
 
-
   (* for f*cks sake, why cant I have some statement for remaining cases? *)
   |"congru (\<acute>a`) Null        = False"
   |"congru Null (\<acute>b`)        = False"
@@ -78,23 +86,33 @@ where
   |"congru (_\<leftarrow>_._) Null     = False"
   |"congru Null (_\<leftarrow>_._)     = False"
 
-  (* eq checks wether two lists have the same ammount of structural congruent objects *)
-  |"eq [] []   = True"
-  |"eq [] (x#xs) = eq (x#xs) []"
-  |"eq (x#xs) bs = (if (inC x bs) then (eq xs (lminus x bs)) else False)"
+  |"eq2 [] [] [] = True"
+  |"eq2 (x#xs) [] [] = False"
+  |"eq2 [] [] (a#as) = False"
+  |"eq2 (x#xs) [] (b#bs) = False"
+  |"eq2 [] (b#bs) _ = False"
+  |"eq2 (x#xs) (y#ys) zs = (if (x =C y) then (eq2 xs (zs@ys) []) else (eq2 (x#xs) ys (y#zs)))"
 
-  (*inC a b checks wether a is in b*)
-  |"inC a [] = (a =C Null)"
-  |"inC a (x#xs) = ((a =C x) \<or> (inC a xs))"
+  |"eq a b = eq2 a b []"
 
-  (* lminus a b removes first element from b which is structural congruent to a*)
-  |"lminus a [] = []"
-  |"lminus a (x#xs) = (if (a =C x) then xs else (x#(lminus a xs)))"
 sorry
 termination
 sorry
 
+
 value "(( \<^bold>0 \<parallel> (n\<^sub>1\<triangleleft>\<^bold>0 \<parallel> \<^bold>0\<triangleright>)) =C (n\<^sub>1\<triangleleft>\<^bold>0\<triangleright>))"
+
+theorem eqTransitive:
+  shows "transitive eq"
+sorry
+
+theorem eqReflexive:
+  shows "reflexive eq"
+sorry
+
+theorem eqSymmetric:
+  shows "symmetric eq"
+sorry
 
 theorem congruTransitive:
   shows "transitive congru"
@@ -110,7 +128,7 @@ sorry
 
 theorem parAssoc:
   shows "((a\<parallel>b)\<parallel>c) =C (a\<parallel>(b\<parallel>c))"
-sorry
+by (metis append_assoc congru.simps(1) congruReflexive getList.simps(2))
 
 lemma eq_congruence: "eq a a"
 sorry
@@ -129,6 +147,10 @@ theorem zeroRight:
 by (smt append_self_conv congru.simps(1) congru.simps(10) congru.simps(13) congru.simps(5) congru.simps(6) congru.simps(9) congruReflexive congruSymmetric getList.elims getList.simps(1) getList.simps(2))
 *)
 sorry
+theorem yeroright:
+  shows "a =C Null \<parallel> a"
+by (simp add: congruSymmetric zeroLeft)
+
 
 theorem someshit:
 shows "(((zero\<leftarrow>zero.\<^bold>0) \<parallel> \<^bold>0) \<parallel> (zero\<leftarrow>zero.\<^bold>0)) =C (((zero\<leftarrow>zero.\<^bold>0) \<parallel> (zero\<leftarrow>zero.\<^bold>0)) \<parallel> \<^bold>0)"
@@ -138,26 +160,23 @@ value "getList ((a \<parallel> b) \<parallel> c)"
 value "(((zero\<leftarrow>zero.\<^bold>0) \<parallel> \<^bold>0) \<parallel> (zero\<leftarrow>zero.\<^bold>0)) =C (((zero\<leftarrow>zero.\<^bold>0) \<parallel> (zero\<leftarrow>zero.\<^bold>0)) \<parallel> \<^bold>0)"
 
 
-
-(*
-normalization "(Null \<parallel> Null) =C Null"
-value[nbe] "(Null\<parallel> Null) =C (Null \<parallel> Null)"
-
-*)
-
-
 (*Name equivalence*)
-function name_equivalence :: "n \<Rightarrow> n \<Rightarrow> bool" (infix "=N" 52)
-  where "`\<acute>x`\<acute> =N y = (x = y)"
+fun name_equivalence :: "n \<Rightarrow> n \<Rightarrow> bool" (infix "=N" 52)
+  where 
+    "`\<acute>x`\<acute> =N y = (x =N y)"
+  | "x =N `\<acute>y`\<acute> = (x =N y)"
   | "zero =N `y\<acute> = (Null =C y)"
+  | "`y\<acute> =N zero = (Null =C y)"
   | "((` (Input x y P) \<acute>) =N `z\<acute>) = ((Input x y P)  =C z)"
+  | "(`z\<acute> =N (` (Input x y P) \<acute>)) = ((Input x y P)  =C z)"
   | "((` P \<parallel> Q \<acute>) =N `z\<acute>) = ( P \<parallel> Q  =C z)"
+  | "( `z\<acute> =N(` P \<parallel> Q \<acute>)) = ( P \<parallel> Q  =C z)"
   | "((` x\<triangleleft>P\<triangleright> \<acute>) =N `z\<acute>) = (x\<triangleleft>P \<triangleright> =C z)"
-  apply pat_completeness
-by auto
-termination
-  by lexicographic_order
+(*  | "( `z\<acute> =N (` x\<triangleleft>P\<triangleright> \<acute>)) = (x\<triangleleft>P \<triangleright> =C z)"*)
 
+
+value "`\<acute>zero`\<acute> =N zero"
+value "`\<acute>`\<acute>zero`\<acute>`\<acute> =N zero"
 
 theorem name_equivalence_reflexive:
   shows "reflexiveR name_equivalence"
@@ -224,6 +243,14 @@ abbreviation three :: P
 abbreviation fore :: P
   where "fore \<equiv> \<acute>newName 4`"
 
+value "three"
+value "newName 3"
+value "`three\<acute>"
+value "newName 3 =N (`three\<acute>)"
+value "newName 3 =N zero"
+value "(`three\<acute>) =N newName 3"
+
+
 value "`((one\<parallel>two)\<parallel>(three\<parallel>fore))\<parallel>Null\<acute> =N `((one\<parallel>three)\<parallel>(two\<parallel>fore))\<acute>"
 
 (*substitution*)
@@ -235,15 +262,15 @@ value "(sn zero zero zero)"
 value "newName (Max ({(n_depth zero), 0::nat}))"
 
 (*with z = (newName (P_depth(R)))*)
-abbreviation z
-  where "z \<equiv> \<lambda> q::n.  \<lambda> p::n. \<lambda> R::P. newName (Max({(n_depth(q)), (P_depth(R)), (n_depth(p)) }))"
+abbreviation genz
+  where "genz \<equiv> \<lambda> q::n.  \<lambda> p::n. \<lambda> R::P. newName (Max({(n_depth(q)), (P_depth(R)), (n_depth(p)) }))"
   
 
 function s :: "P \<Rightarrow> n \<Rightarrow> n \<Rightarrow> P" ("(_) {_\<setminus>_}" 52)
 where "(\<^bold>0){_\<setminus>_}             = \<^bold>0"
    | "(R \<parallel> S){q\<setminus>p}          = ((R){q\<setminus>p}) \<parallel> ((S){q\<setminus>p})" 
-   | "( x \<leftarrow> y . R){q\<setminus>p}    = ((sn x q p) \<leftarrow> (z q p R)  . ((R {(z q p R)\<setminus>y}){q\<setminus>p}))"  
-   | "(x \<triangleleft> R \<triangleright>) {q\<setminus>p}       = (sn x q p) \<triangleleft>R{q\<setminus>p}\<triangleright>"
+   | "( x \<leftarrow> y . R){q\<setminus>p}    = ((sn x q p) \<leftarrow> (genz q p R)  . ((R {(genz q p R)\<setminus>y}){q\<setminus>p}))"  
+   | "(x \<triangleleft> R \<triangleright>) {q\<setminus>p}       = ((sn x q p) \<triangleleft>R{q\<setminus>p}\<triangleright>)"
    | "(\<acute>x`){q\<setminus>p}            = (if (x =N p) then \<acute>q` else \<acute>x`)"
 apply pat_completeness by auto
 termination
@@ -253,10 +280,35 @@ value "\<^bold>0{zero\<setminus>zero}"
 value "\<acute>zero` {`two\<acute>\<setminus> zero}"
 value "(\<^bold>0 \<parallel> (\<acute>zero`)) { (newName 2) \<setminus> zero }"
 
+(*
+ 2.6 Dynamic quote: an example
+*)
+
+value "zero\<triangleleft>zero[[`three\<acute>]]\<triangleright>{`two\<acute>\<setminus>`three\<acute>}"
+value "(zero\<triangleleft>zero[[`three\<acute>]]\<triangleright>{`two\<acute>\<setminus>`three\<acute>}) = (zero\<triangleleft>zero[[`two\<acute>]]\<triangleright>)"
+
+value "zero[[`zero[[`three\<acute>]]\<acute>]]{`two\<acute>\<setminus>`three\<acute>}"
+value "zero[[`zero[[`three\<acute>]]\<acute>]]{`two\<acute>\<setminus>`three\<acute>} = (zero[[`zero[[`three\<acute>]]\<acute>]])"
+
+(*
+lemma "(zero\<triangleleft>zero[[`three\<acute>]]\<triangleright>{`two\<acute>\<setminus>`three\<acute>}) = (zero\<triangleleft>zero[[`two\<acute>]]\<triangleright>)"
+apply simp
+apply auto
+apply (simp add: numeral_3_eq_3)
+proof -
+  have 1:"\<not>(zero =N newName 3)" by (simp add: numeral_3_eq_3)
+  moreover have 2: "newName 3 =N `three\<acute>" by (simp add: numeral_3_eq_3)
+  moreover have 3: "\<not> newName 2 = newName 3" by (simp add: numeral_3_eq_3 numeral_2_eq_2)
+
+how to prove this?
+*)
+
+
+
+
 theorem testerrr:
 shows "(\<^bold>0 \<parallel> (\<acute>zero`)) { (newName 2) \<setminus> zero } = (\<^bold>0 \<parallel> (\<acute>(newName 2)`))"
 by simp
-
 
 (*theory is consistent, yay*)
 lemma False
